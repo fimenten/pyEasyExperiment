fe13a30e7c48af320acc5a7a90b73ae704187fcd
Your Name <you@example.com>
2025-08-29 14:25:26 +0900

    aaa

---
+++ pyeasyexperiment/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyEasyExperiment - A simple experiment tracking library
+"""
+
+from .easy_experiment import EasyExperiment, EasyExperiment2
+
+__version__ = "0.1.0"
+__all__ = ["EasyExperiment", "EasyExperiment2"]
